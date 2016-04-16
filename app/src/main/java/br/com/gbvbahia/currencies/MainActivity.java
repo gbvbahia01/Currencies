9bc5058dd2906c27a2031e95e28bf9d3605aa91c
Guilherme Bahia <gbvbahia01@gmail.com>
2016-04-16 10:27:22 -0300

    Creates options menu

--- app/src/main/java/br/com/gbvbahia/currencies/MainActivity.java
+++ app/src/main/java/br/com/gbvbahia/currencies/MainActivity.java
@@ -2,6 +2,7 @@ package br.com.gbvbahia.currencies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -25,6 +26,39 @@ public class MainActivity extends AppCompatActivity {
 
     referenceViewObjects();
     fillCurrencies();
+  }
+
+  /**
+   * This hook is called whenever an item in your options menu is selected.
+   * The default implementation simply returns false to have the normal
+   * processing happen (calling the item's Runnable or sending a message to
+   * its Handler as appropriate).  You can use this method for any items
+   * for which you would like to do processing without those other
+   * facilities.
+   * <p/>
+   * <p>Derived classes should call through to the base class for it to
+   * perform the default menu handling.</p>
+   *
+   * @param item The menu item that was selected.
+   * @return boolean Return false to allow normal menu processing to
+   * proceed, true to consume it here.
+   * @see #onCreateOptionsMenu
+   */
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    int id = item.getItemId();
+    switch(id){
+      case R.id.mnu_invert:
+        //TODO
+        break;
+      case R.id.mnu_codes:
+        //TODO
+        break;
+      case R.id.mnu_exit:
+        finish();
+        break;
+    }
+    return true;
   }
 
   private void fillCurrencies() {
