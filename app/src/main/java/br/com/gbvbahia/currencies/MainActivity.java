6cc793c4eca63263dc71a359fb49531a7288e201
Guilherme Bahia <gbvbahia01@gmail.com>
2016-04-17 12:42:39 -0300

    Creates launcher icon

--- app/src/main/java/br/com/gbvbahia/currencies/MainActivity.java
+++ app/src/main/java/br/com/gbvbahia/currencies/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -43,6 +44,10 @@ public class MainActivity extends AppCompatActivity
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    ActionBar actionBar = getSupportActionBar();
+    actionBar.setHomeButtonEnabled(true);
+    actionBar.setDisplayShowHomeEnabled(true);
+    actionBar.setIcon(R.mipmap.ic_launcher);
 
     referenceViewObjects();
     fillCurrencies();
